d09a2fa7628414b2665622cfc297346d7a254b6a
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-29 14:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function setSizeText(width, height) {
+  const sizeHeading = document.getElementById("sizeHeading");
+  sizeHeading.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Function to update the size information
+function updateSizeInfo() {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  setSizeText(windowWidth, windowHeight);
+}
+
+// Initial update
+updateSizeInfo();
+
+// Add event listener for window resize
+window.addEventListener("resize", updateSizeInfo);
